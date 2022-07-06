01e40608b402b4534073e29195fb46c1919014bb
Matheus Felipe <theusf40@hotmail.com>
2022-07-06 15:40:48 -0300

    Added Info CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import Info from './components/Info'
+import { createRoot } from "react-dom/client";
+import Info from "./components/Info";
+import About from "./components/About";
+import Interests from "./components/Interests";
+import Footer from "./components/Footer";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 
-function App () {
-    return (
-        <div>
-            <Info />
-        </div>
-    )
+function App() {
+  return (
+    <div>
+      <Info />
+      <About />
+    </div>
+  );
 }
-export default App
+export default App;
 
-root.render(<App />)
+root.render(<App />);
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,21 @@
-import profile from '../../img/profile-picture.png'
+import profile from "../../img/profile-picture.png";
 
 export default function Info() {
-    return (
-        <div>
-            <img className='img' src={profile} />
-            <h1 className='name'>Matheus Felipe</h1>
-            <h3 className='position'>Developer</h3>
-            <a className='website' href='https://www.google.com' target='blank'>Random link</a>
-            <button className='email'>E-mail</button>
-            <button className='linkedIn'>LinkedIn</button>
-        </div>
-    )
+  return (
+    <div>
+      <img className="img" src={profile} />
+      <h1 className="name text">Matheus Felipe</h1>
+      <h3 className="position text">Developer</h3>
+      <a className="website text" href="https://www.google.com" target="blank">
+        Random link
+      </a>
+      <div className="btn-container">
+        <button className="email btn">
+          <i class="fa-solid fa-envelope"></i> E-mail
+        </button>
+        <button className="linkedin btn">
+        <i class="fa-brands fa-linkedin"></i> LinkedIn</button>
+      </div>
+    </div>
+  );
 }
