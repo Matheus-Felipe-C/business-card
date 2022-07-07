a1cee2cb86b0b7e39cc3bd5a7d773b68901c3003
Matheus Felipe de Oliveira Ferreira <82980047+Matheus-Felipe-C@users.noreply.github.com>
2022-07-07 22:25:31 +0000

    added socials on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import github from '../../img/github.svg'
+import instagram from '../../img/instagram-square.svg'
+import twitter from '../../img/twitter-square.svg'
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <i className="fa-brands fa-twitter-square"></i>
-            <i className="fa-brands fa-instagram-square"></i>
-            <img className='' src={github}/>
+            <a className="footer-icon"  href="https://twitter.com/theusf40">
+              <img src={twitter} />
+            </a>
+            <a className="footer-icon" href="https://www.instagram.com/theusf40/">
+              <img src={instagram} />
+            </a>
+            <a className='footer-icon' href="https://github.com/Matheus-Felipe-C">
+              <img src={github}/>
+            </a>
         </footer>
     )
 }
